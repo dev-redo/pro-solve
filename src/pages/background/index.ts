fd86feda3561a54a14b23d978e8889509e390bcb
dev-redo <mis05041@naver.com>
2022-09-13 04:36:06 +0900

    refactor: testPage 리펙토링

--- src/pages/background/index.ts
+++ src/pages/background/index.ts
@@ -5,8 +5,22 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.method === 'postCurrentSolution') {
     (async () => {
       try {
-        const cityRef = doc(db, 'cities', 'BJ');
-        const response = await setDoc(cityRef, { capital: true });
+        const { isSuccess, code, selectedLanguage, problemId, passedTestCase, failedTestCase } =
+          request.data;
+        console.log('[Pro Solve] ', request);
+        const uid = auth.currentUser.uid;
+
+        // codingTest/{uid}/{문제id}/{language}
+        const codingTestRef = doc(db, 'codingTest', uid, problemId, selectedLanguage);
+        await setDoc(codingTestRef, {
+          isSuccess,
+          code,
+          passedTestCase,
+          failedTestCase,
+          uploadTime: new Date(),
+        });
+
+        console.log('[Pro Solve] 업로드 성공!');
         sendResponse({ status: true });
       } catch (error) {
         console.log('[Pro Solve] 로그인을 하지 않아 업로드가 되지 않습니다!', error);
--- src/pages/content/testPage.ts
+++ src/pages/content/testPage.ts
@@ -1,49 +1,38 @@
 import { theme } from '../../styles/theme';
 
-const submitBtn = document.querySelector('#submit-code');
+const $submitBtn = document.querySelector('#submit-code');
 const $modal = document.querySelector('.modal');
-const modalMutationObserver = new MutationObserver(mutations => {
-  if (!!mutations.length) {
-    readyToUpload();
+const modalMutationOption = {
+  childList: true,
+};
 
-    const isFirstAnswer = !!document.querySelector('a#view-solution-group > svg');
-    uploadCurrentSolution(isFirstAnswer);
-    modalMutationObserver.disconnect();
-  }
+const modalMutationObserver = new MutationObserver(mutations => {
+  if (!mutations.length) return;
+
+  readyToUpload();
+  uploadCurrentSolution();
+  modalMutationObserver.disconnect();
 });
 
-submitBtn.addEventListener('click', () => {
-  modalMutationObserver.observe($modal, {
-    childList: true,
-  });
+$submitBtn.addEventListener('click', () => {
+  modalMutationObserver.observe($modal, modalMutationOption);
 });
 
 const readyToUpload = () => {
-  const modalContent = document.querySelector('div.modal-body');
-  modalContent.textContent = 'Pro Solve 익스텐션이 결과를 저장합니다.';
+  const $modalContent = document.querySelector<HTMLElement>('div.modal-body');
+  $modalContent.textContent = 'Pro Solve 익스텐션이 결과를 저장합니다.';
 
   const modalUploadResult = document.createElement('div');
   modalUploadResult.className = 'modal-upload';
   modalUploadResult.textContent = 'Loading...';
   modalUploadResult.style.color = theme.color.deepBlue;
-  modalContent.append(modalUploadResult);
+  $modalContent.append(modalUploadResult);
 };
 
-const uploadCurrentSolution = async (isFirstAnswer: boolean) => {
-  const domNode = parsingDomNodeToUpload();
-  // const {
-  //   solutionResult,
-  //   isSuccess,
-  //   code,
-  //   selectedLanguage,
-  //   problemId,
-  //   passedTestCase,
-  //   failedTestCase,
-  // } = parsingDomNodeToUpload();
-  console.log('[Pro Solve] 풀이 여부 :>> ', isFirstAnswer);
-
+const uploadCurrentSolution = async () => {
+  const data = parsingDomNodeToUpload();
   const uploadResult = await new Promise<boolean>(resolve => {
-    chrome.runtime.sendMessage({ method: 'postCurrentSolution' }, response => {
+    chrome.runtime.sendMessage({ method: 'postCurrentSolution', data }, response => {
       resolve(response.status);
       console.log('[Pro Solve] 코드 업로드 성공 여부 :>>', response.status);
     });
@@ -53,21 +42,20 @@ const uploadCurrentSolution = async (isFirstAnswer: boolean) => {
 };
 
 const parsingDomNodeToUpload = () => {
-  const solutionResult = document.querySelector('div.modal-header > h4').textContent; // 풀이 결과: 정답입니다 or 틀렸습니다!
-  const isSuccess = solutionResult.includes('정답') ? true : false; // 정답 여부
+  const solutionResult = document.querySelector('div.modal-header > h4').textContent;
+  const isSuccess = solutionResult.includes('정답');
 
-  const code = (document.querySelector('textarea#code') as HTMLTextAreaElement).value; // 제출한 풀이
-  const selectedLanguage = (
-    document.querySelector('div.editor > ul > li.nav-item > a') as HTMLAnchorElement
-  ).getAttribute('data-language'); // 풀이한 언어
+  const code = document.querySelector<HTMLTextAreaElement>('textarea#code').value;
+  const selectedLanguage = document
+    .querySelector<HTMLAnchorElement>('div.editor > ul > li.nav-item > a')
+    .getAttribute('data-language');
   const problemId = document
     .querySelector('div.main > div.lesson-content')
     .getAttribute('data-lesson-id');
-  const passedTestCase = document.querySelectorAll('td.result.passed').length; // 성공한 테케 개수
-  const failedTestCase = document.querySelectorAll('td.result.failed').length; // 실패한 테케 개수
+  const passedTestCase = document.querySelectorAll('td.result.passed').length;
+  const failedTestCase = document.querySelectorAll('td.result.failed').length;
 
   return {
-    solutionResult,
     isSuccess,
     code,
     selectedLanguage,
@@ -78,13 +66,14 @@ const parsingDomNodeToUpload = () => {
 };
 
 const printIsUploadSuccess = (uploadResult: boolean) => {
-  const modalUploadResult = document.querySelector<HTMLElement>('div.modal-upload');
+  const $modalUploadResult = document.querySelector<HTMLElement>('div.modal-upload');
 
   if (uploadResult) {
-    modalUploadResult.textContent = '업로드 성공!';
-    modalUploadResult.style.color = theme.color.blue;
-  } else {
-    modalUploadResult.textContent = '업로드 중 에러가 발생했습니다. 로그인 여부를 확인해주세요!';
-    modalUploadResult.style.color = theme.color.red;
+    $modalUploadResult.textContent = '업로드 성공!';
+    $modalUploadResult.style.color = theme.color.blue;
+    return;
   }
+
+  $modalUploadResult.textContent = '업로드 중 에러가 발생했습니다. 로그인 여부를 확인해주세요!';
+  $modalUploadResult.style.color = theme.color.red;
 };
