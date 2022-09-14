034bba69439cc19c9dd68b5718a5a9db31bc8d21
dev-redo <mis05041@naver.com>
2022-09-14 13:00:28 +0900

    feat: type 지정

--- src/pages/content/testPage.ts
+++ src/pages/content/testPage.ts
@@ -1,7 +1,7 @@
 import { theme } from '../../styles/theme';
 
-const $submitBtn = document.querySelector('#submit-code');
-const $modal = document.querySelector('.modal');
+const $submitBtn = document.querySelector('#submit-code') as HTMLButtonElement;
+const $modal = document.querySelector('.modal') as HTMLDivElement;
 const modalMutationOption = {
   childList: true,
 };
@@ -19,8 +19,8 @@ $submitBtn.addEventListener('click', () => {
 });
 
 const readyToUpload = () => {
-  const $modalContent = document.querySelector<HTMLElement>('div.modal-body');
-  $modalContent.textContent = 'Pro Solve 익스텐션이 결과를 저장합니다.';
+  const $modalContent = document.querySelector('div.modal-body') as HTMLDivElement;
+  $modalContent.innerHTML = `<span>Pro Solve 익스텐션이 결과를 저장합니다.<br />모달 창을 닫으셔도 됩니다.</span>`;
 
   const modalUploadResult = document.createElement('div');
   modalUploadResult.className = 'modal-upload';
@@ -30,6 +30,8 @@ const readyToUpload = () => {
 };
 
 const uploadCurrentSolution = async () => {
+  console.log('[Pro Solve] 제출한 코드 업로드를 시작합니다.');
+
   const data = parsingDomNodeToUpload();
   const uploadResult = await new Promise<boolean>(resolve => {
     chrome.runtime.sendMessage({ method: 'postCurrentSolution', data }, response => {
@@ -42,16 +44,17 @@ const uploadCurrentSolution = async () => {
 };
 
 const parsingDomNodeToUpload = () => {
-  const solutionResult = document.querySelector('div.modal-header > h4').textContent;
-  const isSuccess = solutionResult.includes('정답');
+  const solutionResult = (document.querySelector('div.modal-header > h4') as HTMLHeadElement)
+    .textContent;
+  const isSuccess = solutionResult!.includes('정답');
 
-  const code = document.querySelector<HTMLTextAreaElement>('textarea#code').value;
-  const selectedLanguage = document
-    .querySelector<HTMLAnchorElement>('div.editor > ul > li.nav-item > a')
-    .getAttribute('data-language');
-  const problemId = document
-    .querySelector('div.main > div.lesson-content')
-    .getAttribute('data-lesson-id');
+  const code = (document.querySelector('textarea#code') as HTMLTextAreaElement).value;
+  const selectedLanguage = (
+    document.querySelector('div.editor > ul > li.nav-item > a') as HTMLAnchorElement
+  ).getAttribute('data-language');
+  const problemId = (
+    document.querySelector('div.main > div.lesson-content') as HTMLDivElement
+  ).getAttribute('data-lesson-id');
   const passedTestCase = document.querySelectorAll('td.result.passed').length;
   const failedTestCase = document.querySelectorAll('td.result.failed').length;
 
@@ -66,7 +69,7 @@ const parsingDomNodeToUpload = () => {
 };
 
 const printIsUploadSuccess = (uploadResult: boolean) => {
-  const $modalUploadResult = document.querySelector<HTMLElement>('div.modal-upload');
+  const $modalUploadResult = document.querySelector('div.modal-upload') as HTMLElement;
 
   if (uploadResult) {
     $modalUploadResult.textContent = '업로드 성공!';
