382270f7e034c8c31c741fc78bd9e2ecd9785567
hsy <69149030+dev-redo@users.noreply.github.com>
2022-09-17 02:25:02 +0900

    feat: 제출한 모든 풀이를 보는 tab 생성 (#15)
    * style: 이미지, 폰트, 아이콘 폴더 이동
    
    * style: yarn.lock 파일을 gitignore파일에서 제거
    
    * fix: svg 파일 에러 해결
    
    asset/resource 속성으로 svg파일이 url로 내보내져 렌더링 이슈가 발생했다.
    해당 속성을 제거함으로써 이슈를 해결하였다.
    
    * feat: popup 퍼블리싱 완료
    
    * feat: hook 분리
    
    구글 로그인과 풀이 데이터 fetch로직을 분리하였다.
    
    * feat: 제출한 모든 풀이를 보는 tab 생성

--- src/pages/content/solutionPage.tsx
+++ src/pages/content/solutionPage.tsx
@@ -1,45 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import styled from 'styled-components';
 import { ThemeProvider } from 'styled-components';
 import { theme } from '../../styles/theme';
-import GlobalStyles from '../../styles/global';
-import { SuccessResponse, FailedResponse } from '../../types/code';
-import { getAllSolutions } from '../../hooks/getAllSolutions';
 
-function SolutionPage() {
-  // TODO: Solution type 지정
-  const [solutions, setSolutions] = React.useState<SuccessResponse | FailedResponse>({});
+const languageRegex = /(?<=language=\s*)\w*(?=\&type=)/g;
+const problemIdRegex = /lessons\/(.+?)\/solution/;
 
-  React.useEffect(() => {
-    (async () => {
-      const allSolutions = await getAllSolutions();
-      setSolutions(allSolutions);
-    })();
-  }, []);
+const href = window.location.href;
+const selectedLanguage = href.match(languageRegex)![0];
+const [_, problemId] = href.match(problemIdRegex)!;
+const problemName = ([...document.querySelectorAll('ol.breadcrumb > li')] as HTMLElement[])[2]
+  .innerText;
 
-  // 로딩중 = 스켈레톤 ui (마지막에 구현)
+const createSolutionTab = () => {
+  chrome.runtime.sendMessage({
+    method: 'newTab',
+    href: { selectedLanguage, problemId, problemName },
+  });
+};
 
-  // [로그인 X]: status = false
-  // 내가 제출한 코드 풀이테이블 + 로그인을 해주세요 문구 띄우기
+const SolutionPage = () => {
+  return <SolutionStyle onClick={createSolutionTab}>제출한 모든 풀이</SolutionStyle>;
+};
 
-  // [로그인 O]
-  // 풀이 테이블 띄우기
+const SolutionStyle = styled.a`
+  padding: 0.3125rem 0.8125rem;
+  font-size: 0.875rem;
+  color: ${props => props.theme.color.white};
+`;
 
-  return (
-    <div>
-      <span>Solution</span>
-      <span>{JSON.stringify(solutions)}</span>
-    </div>
-  );
-}
+const root = document.querySelector('div.result-tab > div#tab') as HTMLDivElement;
+const contentScript = document.createElement('button');
+contentScript.style.backgroundColor = theme.color.darkGrey;
+contentScript.style.marginLeft = '0.85rem';
+contentScript.style.borderRadius = '0.25rem';
 
-const root = document.querySelector('div.main > div.container') as HTMLDivElement;
-const contentScript = document.createElement('div');
 root.appendChild(contentScript);
 ReactDOM.createRoot(contentScript as HTMLElement).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <GlobalStyles />
       <SolutionPage />
     </ThemeProvider>
   </React.StrictMode>,
