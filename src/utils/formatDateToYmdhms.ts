379f93f0182a2341a2360291c4febff3ba87963c
hsy <69149030+dev-redo@users.noreply.github.com>
2022-09-19 03:36:01 +0900

    fix: 날짜 포맷 이슈 해결 (#18)
    기존에는 ISO 기준으로 해서 우리나라보다 하루 늦은 시간으로 포맷팅되었다.
    이를 Locale로 변경해 해결하였다.

--- src/utils/formatDateToYmdhms.ts
+++ src/utils/formatDateToYmdhms.ts
@@ -1,11 +1,10 @@
 const formatDateToYmdhms = (date: Date) => {
-  const [yyyy, mm, dd] = date.toISOString().split('T')[0].split('-');
-  const [h, m, s] = date.toTimeString().split(' ')[0].split(':');
+  const ymdRegex = /(\d\w*)/g;
+  const [yyyy, mm, dd] = date.toLocaleDateString().match(ymdRegex)!;
+  const [amPm, hhmmss] = date.toLocaleTimeString().split(' ');
+  const [h, m, s] = hhmmss.split(':');
 
-  const amPm = Number(h) < 12 ? '오전' : '오후';
-  const formatMonth = Number(h) < 12 ? Number(h) : Number(h) - 12;
-
-  return `${yyyy}년 ${Number(mm)}월 ${dd}일 ${amPm} ${formatMonth}시 ${m}분`;
+  return `${yyyy}년 ${mm}월 ${dd}일 ${amPm} ${h}시 ${m}분`;
 };
 
 export { formatDateToYmdhms };
