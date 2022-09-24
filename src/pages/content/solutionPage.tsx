a51fccb62c45ec35847556f77c41fff94d9eb0c9
dev-redo <mis05041@naver.com>
2022-09-24 22:04:57 +0900

    feat: 코드 제출 시 띄워지는 모달에 저장된 풀이 버튼 생성

--- src/pages/content/solutionPage.tsx
+++ src/pages/content/solutionPage.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import styled from 'styled-components';
 import { ThemeProvider } from 'styled-components';
 import { theme } from '../../styles/theme';
+import ShowSolutionsButtons from '../../components/button/showSolutionsButton';
 
 const languageRegex = /(?<=language=\s*)\w*/g;
 const problemIdRegex = /(?<=lessons\/\s*)\w+/g;
@@ -14,37 +14,20 @@ const problemId = href.match(problemIdRegex)![0];
 const problemName = ([...document.querySelectorAll('ol.breadcrumb > li')] as HTMLElement[])[2]
   .innerText;
 
-const createSolutionTab = () => {
-  chrome.runtime.sendMessage({
-    method: 'newTab',
-    href: { selectedLanguage, problemId, problemName },
-  });
-};
-
 const SolutionPage = () => {
   console.log(`언어 | 문제 번호 | 문제 이름 >> ${selectedLanguage}, ${problemId}, ${problemName}`);
 
-  return <SolutionStyle onClick={createSolutionTab}>제출한 모든 풀이</SolutionStyle>;
+  return (
+    <ShowSolutionsButtons
+      selectedLanguage={selectedLanguage}
+      problemId={problemId}
+      problemName={problemName}
+    />
+  );
 };
 
-const SolutionStyle = styled.span`
-  padding: 0.3125rem 0.8125rem;
-  font-size: 0.875rem;
-  color: ${props => props.theme.color.white};
-  &:hover {
-    color: ${props => props.theme.color.white};
-  }
-`;
-
 const root = document.querySelector('div.result-tab > div#tab') as HTMLDivElement;
-const contentScript = document.createElement('button');
-contentScript.style.cssText = `
-  background-color: ${theme.color.darkGrey};
-  margin-left: 0.85rem;
-  border-radius: 0.25rem;
-  border: none;
-`;
-
+const contentScript = document.createElement('div');
 root.appendChild(contentScript);
 ReactDOM.createRoot(contentScript as HTMLElement).render(
   <React.StrictMode>
