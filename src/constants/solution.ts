55f1a0dd02a472f6e9c516c3f668c8a0fadfce84
dev-redo <mis05041@naver.com>
2022-10-01 03:00:42 +0900

    feat:저장한 풀이 페이지에 넣을 select 박스들의 lookup table 생성

---
+++ src/constants/solution.ts
@@ -0,0 +1,12 @@
+const SOLUTION_TYPE = {
+  ALL: '전체 풀이',
+  SUCCESS: '성공한 풀이',
+  FAILED: '실패한 풀이',
+};
+
+const SORT_TYPE = {
+  ASC: '오름차순',
+  DESC: '내림차순',
+};
+
+export { SOLUTION_TYPE, SORT_TYPE };
