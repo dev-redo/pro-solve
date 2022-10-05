8a3009750d3fc24951d59895f5f6f8ea3999a776
hsy <69149030+dev-redo@users.noreply.github.com>
2022-10-05 15:32:34 +0900

    style: 로직을 함수로 분리 (#41)
    * feat: firebase timestamp를 date 객체로 변경하는 기능을 util로 분리
    
    * feat: 새로고침 버튼에 쉐도우 추가
    
    * style: console.log 제거
    
    * feat:저장한 풀이 페이지에 넣을 select 박스들의 lookup table 생성
    
    * feat: 개발을 위해 manifest 공개키 key 추가
    
    * style: 로직을 함수로 분리

--- src/components/section/CloseBoxOnOutside.tsx
+++ src/components/section/CloseBoxOnOutside.tsx
@@ -5,31 +5,31 @@ interface ComponentProps {
   children: JSX.Element | JSX.Element[];
 }
 
+const CloseBoxOnOutside = ({ onClose, children }: ComponentProps) => {
+  const wrapRef = React.useRef<HTMLDivElement>(null);
+  useCloseBoxOnOutside({ ref: wrapRef, onClose });
+
+  return <div ref={wrapRef}>{children}</div>;
+};
+
 interface HookProps {
   ref: React.RefObject<HTMLDivElement>;
   onClose: () => void;
 }
 
-const CloseBoxOnOutside = ({ onClose, children }: ComponentProps) => {
-  const useCloseBoxOnOutside = ({ ref, onClose }: HookProps) => {
-    React.useEffect(() => {
-      const handleClickOutside = (event: MouseEvent) => {
-        if (ref.current && !ref.current.contains(event.target as Node)) {
-          onClose();
-        }
-      };
+const useCloseBoxOnOutside = ({ ref, onClose }: HookProps) => {
+  React.useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (ref.current && !ref.current.contains(event.target as Node)) {
+        onClose();
+      }
+    };
 
-      document.addEventListener('click', handleClickOutside);
-      return () => {
-        document.removeEventListener('click', handleClickOutside);
-      };
-    }, [ref]);
-  };
-
-  const wrapRef = React.useRef<HTMLDivElement>(null);
-  useCloseBoxOnOutside({ ref: wrapRef, onClose });
-
-  return <div ref={wrapRef}>{children}</div>;
+    document.addEventListener('click', handleClickOutside);
+    return () => {
+      document.removeEventListener('click', handleClickOutside);
+    };
+  }, [ref]);
 };
 
 export default CloseBoxOnOutside;
--- src/pages/background/index.ts
+++ src/pages/background/index.ts
@@ -12,89 +12,97 @@ const getCurrentUser: GetCurrentUserFn = () => {
   );
 };
 
+type Message = {
+  request: any;
+  sender: chrome.runtime.MessageSender;
+  sendResponse: Function;
+};
+
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  const message = { request, sender, sendResponse };
+
   if (request.method === 'postCurrentSolution') {
-    (async () => {
-      try {
-        console.log('[Pro Solve] request :>>', request);
-        const { isSuccess, code, selectedLanguage, problemId, passedTestCase, failedTestCase } =
-          request.data;
-        const uploadTime = Timestamp.now();
-        const { uid } = (await getCurrentUser()) as User;
+    postCurrentSolution(message).catch(error => {
+      console.log('[Pro Solve] 로그인을 하지 않아 업로드가 되지 않습니다!', error);
+      sendResponse({ status: false });
+    });
 
-        const codingTestRef = doc(db, 'codingTest', uid, problemId, String(uploadTime));
-        await setDoc(codingTestRef, {
-          isSuccess,
-          code,
-          passedTestCase,
-          failedTestCase,
-          selectedLanguage,
-          uploadTime,
-        });
-
-        console.log('[Pro Solve] 업로드 성공!');
-        sendResponse({ status: true });
-      } catch (error) {
-        console.log('[Pro Solve] 로그인을 하지 않아 업로드가 되지 않습니다!', error);
-        sendResponse({ status: false });
-      }
-    })();
     return true;
   }
 
   if (request.method === 'getAllSolutions') {
-    (async () => {
-      try {
-        console.log('[Pro Solve] request :>>', request);
-
-        const { selectedLanguage, problemId } = request.data;
-        const { uid } = (await getCurrentUser()) as User;
-
-        const codingTestRef = collection(db, 'codingTest', uid, problemId);
-        const codingTestQuery = query(
-          codingTestRef,
-          where('selectedLanguage', '==', selectedLanguage),
-        );
-
-        const querySnapshot = await getDocs(codingTestQuery);
-        const data = querySnapshot.docs.map(doc => ({
-          ...doc.data(),
-        }));
-        sendResponse({ status: true, data });
-      } catch (error) {
-        if (error instanceof Error) {
-          console.log('[Pro Solve] 풀이를 가져오던 중 에러가 발생했습니다!', error);
-          sendResponse({ status: false, message: error.message });
-        }
-      }
-    })();
+    getAllSolutions(message).catch(error => {
+      console.log('[Pro Solve] 풀이를 가져오던 중 에러가 발생했습니다!', error);
+      sendResponse({ status: false, message: error.message });
+    });
 
     return true;
   }
 
   if (request.method === 'newTab') {
-    console.log('[Pro Solve] request :>>', request);
-
-    const { selectedLanguage, problemId, problemName } = request.href;
-    const url = `chrome-extension://${chrome.runtime.id}/solutionTab.html?num=${problemId}&name=${problemName}&language=${selectedLanguage}`;
-
-    chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
-      const tabIndex = tabs[0]!.index;
-      chrome.tabs.create({ url, index: tabIndex + 1 });
-    });
-
-    // chrome.windows.getAll({ populate: false, windowTypes: ['normal'] }, windows => {
-    //   for (let window of windows) {
-    //     if (window.incognito) {
-    //       chrome.windows.create({ url, incognito: true });
-    //       return;
-    //     }
-    //   }
-
-    //   chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
-    //     const tabIndex = tabs[0]!.index;
-    //     chrome.tabs.create({ url, index: tabIndex + 1 });
-    //   });
-    // });
+    newTab(message);
   }
 });
+
+const postCurrentSolution = async ({ request, sendResponse }: Message) => {
+  console.log('[Pro Solve] request :>>', request);
+
+  const { isSuccess, code, selectedLanguage, problemId, passedTestCase, failedTestCase } =
+    request.data;
+  const uploadTime = Timestamp.now();
+  const { uid } = (await getCurrentUser()) as User;
+
+  const codingTestRef = doc(db, 'codingTest', uid, problemId, String(uploadTime));
+  await setDoc(codingTestRef, {
+    isSuccess,
+    code,
+    passedTestCase,
+    failedTestCase,
+    selectedLanguage,
+    uploadTime,
+  });
+
+  console.log('[Pro Solve] 업로드 성공!');
+  sendResponse({ status: true });
+};
+
+const getAllSolutions = async ({ request, sendResponse }: Message) => {
+  console.log('[Pro Solve] request :>>', request);
+
+  const { selectedLanguage, problemId } = request.data;
+  const { uid } = (await getCurrentUser()) as User;
+
+  const codingTestRef = collection(db, 'codingTest', uid, problemId);
+  const codingTestQuery = query(codingTestRef, where('selectedLanguage', '==', selectedLanguage));
+
+  const querySnapshot = await getDocs(codingTestQuery);
+  const data = querySnapshot.docs.map(doc => ({
+    ...doc.data(),
+  }));
+
+  sendResponse({ status: true, data });
+};
+
+type Problem = {
+  problemId: string;
+  problemName: string;
+  selectedLanguage: string;
+};
+
+const newTab = ({ request }: Message) => {
+  console.log('[Pro Solve] request :>>', request);
+
+  const problem = request.href;
+  const url = getNewTabUrl(chrome.runtime.id, problem);
+
+  openNewTab(url);
+};
+
+const getNewTabUrl = (runtimeId: string, { problemId, problemName, selectedLanguage }: Problem) =>
+  `chrome-extension://${runtimeId}/solutionTab.html?num=${problemId}&name=${problemName}&language=${selectedLanguage}`;
+
+const openNewTab = (url: string) =>
+  chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+    const tabIndex = tabs[0]!.index;
+    chrome.tabs.create({ url, index: tabIndex + 1 });
+  });
