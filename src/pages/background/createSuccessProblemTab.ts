183faa281536a4d10e818eb016fd7833952fbaab
hsy <69149030+dev-redo@users.noreply.github.com>
2022-10-16 22:48:08 +0900

    feat: 문제 파싱 util함수 구현 (#61)

--- src/pages/background/createSuccessProblemTab.ts
+++ src/pages/background/createSuccessProblemTab.ts
@@ -1,19 +1,11 @@
-import { Message } from '../../types/global';
 import { createChromeTab } from './index';
 
-type UserInfoType = {
-  userName: string;
-  userImg: string;
-};
-
-const createSuccessProblemTab = async ({ request }: Message) => {
-  const { userInfo } = request;
-
-  const url = getAllSuccessProblemTabUrl(chrome.runtime.id, userInfo);
+const createSuccessProblemTab = async () => {
+  const url = getAllSuccessProblemTabUrl(chrome.runtime.id);
   createChromeTab(url);
 };
 
-const getAllSuccessProblemTabUrl = (runtimeId: string, { userName, userImg }: UserInfoType) =>
-  `chrome-extension://${runtimeId}/profileTab.html?name=${userName}&img=${userImg}`;
+const getAllSuccessProblemTabUrl = (runtimeId: string) =>
+  `chrome-extension://${runtimeId}/profileTab.html`;
 
 export { createSuccessProblemTab };
