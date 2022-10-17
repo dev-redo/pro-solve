2b7adaa58b5bbafb8383651a453ef91113ad30c6
hsy <69149030+dev-redo@users.noreply.github.com>
2022-10-18 05:01:47 +0900

    feat: profile tab의 sort bar의 item 퍼블리싱 (#71)

--- src/constants/profile.ts
+++ src/constants/profile.ts
@@ -1,7 +1,16 @@
+import { SelectNameType } from '../types/profile';
+
 const NAV_LIST = ['MAIN', 'PROBLEM'];
 const NAV_TYPE = {
   MAIN: '개요',
   PROBLEM: '문제',
 };
 
-export {NAV_LIST, NAV_TYPE}
+const SORT_LIST: SelectNameType[] = ['level', 'finishedCount', 'acceptanceRate'];
+const SORT_TYPE = {
+  level: '난이도',
+  finishedCount: '완료한 사람',
+  acceptanceRate: '정답률',
+};
+
+export { NAV_LIST, NAV_TYPE, SORT_LIST, SORT_TYPE };
