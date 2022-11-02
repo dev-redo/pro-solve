83a895206b598397c4ec2b9b41d19879bdf1a09e
dev-redo <mis05041@naver.com>
2022-11-03 03:54:57 +0900

    refactor: 성공한 문제 id list를 storage에 저장
    성공한 문제 id list를 맨 처음에만 storage에 저장을 하고, 문제를 풀 때마다 해당 문제의 id를 push한다.
    기존에는 나의 풀이 tab을 생성할 때마다 성공한 문제 list를 fetch하였다.
    하지만 해당 방식을 통해 맨 처음에만 fetch를 하고, local의 storage의 값을 얻어오는 식으로 진행함으로
    나의 풀이 tab 생성 시 리소스를 줄였다.

--- src/api/solution/addSolvedProblemId.ts
+++ src/api/solution/addSolvedProblemId.ts
@@ -1,22 +1,16 @@
-import { getUserEmail } from './getUserEmail';
-
 const addSolvedProblemId = async (id: number, isSuccess: boolean) => {
   if (!isSuccess) return;
 
-  const userEmail = await getUserEmail();
+  const { userEmail } = await chrome.storage.local.get('userEmail');
+  const solvedProblem = (await chrome.storage.local.get([userEmail]))[userEmail];
 
-  chrome.storage.local.get([userEmail], async response => {
-    const solvedProblem = response[userEmail];
-    console.log('[Pro-Solve] 제출한 풀이 Id List :>>', solvedProblem);
+  if (solvedProblem.includes(id)) {
+    console.log(`[Pro-Solve] 이전에 성공한 문제입니다. :>> ${id}`);
+    return;
+  }
 
-    if (solvedProblem.includes(id)) {
-      console.log(`[Pro-Solve] 이전에 성공한 문제입니다. :>> ${id}`);
-      return;
-    }
-
-    chrome.storage.local.set({
-      [userEmail]: [...solvedProblem, id],
-    });
+  return chrome.storage.local.set({
+    [userEmail]: [...solvedProblem, id],
   });
 };
 
