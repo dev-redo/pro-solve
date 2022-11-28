2e30661add4ac2c5e509480d03e3c18f739dc371
dev-redo <mis05041@naver.com>
2022-11-29 05:00:48 +0900

    refactor(problemPage): 나의 풀이 페이지 진입 버튼 스타일링 변경

--- src/pages/content/problemPage.tsx
+++ src/pages/content/problemPage.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import styled from 'styled-components';
 import { ThemeProvider } from 'styled-components';
 import { theme } from '@src/styles/theme';
-import Profile from '@assets/icons/Profile.svg';
+import ProfileLogo from '@assets/icons/ProfileLogo.svg';
 import Modal from '@src/components/modal/Modal';
 import RefreshRequestBox from '@src/components/box/RefreshRequestBox';
 
@@ -18,10 +18,12 @@ const OpenSuccessTabButton = () => {
           새로고침을 해주세요!
         </RefreshRequestBox>
       </Modal>
-      <ButtonStyle onClick={() => createSuccessProblemTab(onOpenModal)}>
-        <ToolTipStyle>나의 풀이 바로가기</ToolTipStyle>
-        <Profile />
-      </ButtonStyle>
+      <ProfileButtonStyle onClick={() => createSuccessProblemTab(onOpenModal)}>
+        <ProfileInfoStyle>
+          <ProfileLogo />
+          <span>나의 풀이 페이지</span>
+        </ProfileInfoStyle>
+      </ProfileButtonStyle>
     </>
   );
 };
@@ -37,7 +39,7 @@ const createSuccessProblemTab = (onOpenModal: () => void) => {
   });
 };
 
-const ButtonStyle = styled.button`
+const ProfileButtonStyle = styled.button`
   display: flex;
   justify-content: space-between;
   align-items: center;
@@ -46,38 +48,30 @@ const ButtonStyle = styled.button`
   right: 5%;
   z-index: 10000;
   border: none;
-  background: none;
-  svg {
-    filter: drop-shadow(#707373 0px 5px 5px);
-    transition: all 0.2s linear;
-  }
-  span {
-    display: none;
-  }
-  &:hover {
-    transform: translateY(-5%);
-    transition: 0.4s ease-in-out;
-    svg {
-      filter: drop-shadow(rgb(154, 156, 160) 0px 5px 5px);
-    }
-    span {
-      display: block;
-    }
-  }
+  font-weight: 600;
 `;
 
-const ToolTipStyle = styled.span`
-  width: 8rem;
-  border: 1px solid;
-  border-radius: 5px;
-  font-size: 0.8em;
-  color: ${({ theme }) => theme.color.white};
-  background-color: ${({ theme }) => theme.color.black};
-  padding: 0.5rem 0;
-  margin-right: 0.5rem;
+const ProfileInfoStyle = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 1.1rem;
+  border-radius: 0.35rem;
+  background: #12191c;
+  color: white;
+  &:hover {
+    background: #00254f;
+  }
+
+  & > span {
+    margin-left: 0.5rem;
+  }
 `;
 
 const btn = document.createElement('a');
+window.addEventListener('load', () => {
+  document.body.prepend(btn);
+});
+
 document.body.prepend(btn);
 ReactDOM.createRoot(btn as HTMLElement).render(
   <React.StrictMode>
