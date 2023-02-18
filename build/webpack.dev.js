477eb1c15323e4543c14710ce322821e2aad67e4
dev-redo <mis05041@naver.com>
2023-02-19 00:02:19 +0900

    refactor: webpack 개발용 설정에 HMR 추가

--- build/webpack.dev.js
+++ build/webpack.dev.js
@@ -1,4 +1,7 @@
+const { HotModuleReplacementPlugin } = require('webpack');
+
 module.exports = {
   mode: 'development',
   devtool: 'eval-cheap-module-source-map',
+  plugins: [new HotModuleReplacementPlugin()],
 };
