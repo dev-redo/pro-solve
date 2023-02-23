6df42ac17d08c03c9495293b6ef84cc7c28e76ce
hsy <69149030+dev-redo@users.noreply.github.com>
2023-02-24 04:17:52 +0900

    feat: 코드가 렌더링되기 전에 보여줄 Skeleton UI 구현 (#138)
    * fix: 나의 풀이 페이지 버튼 위치를 %에서 rem으로 수정
    
    기존에는 %로 버튼을 위치시켰으나, 모니터 해상도에 따라 원하는 위치에 버튼이 있지 않았음
    rem을 이용해 모든 모니터에 대해서 동일한 위치에 있게끔 수정했다.
    
    * fix: 코드 깜빡임 현상 해결
    
    * feat: SkeletonUI 구현
    
    * feat: Virtual Scroll 구현
    
    * feat: code mirror 스타일링

--- src/components/shared/code/CodeMirror.tsx
+++ src/components/shared/code/CodeMirror.tsx
@@ -1,43 +1,46 @@
-import styled from 'styled-components';
+import styled, { createGlobalStyle } from 'styled-components';
 import CopyClipBoardButton from '@src/components/shared/button/CopyClipBoardButton';
+import { Light as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { Solution } from '@src/types/solution';
-import { LightAsync as SyntaxHighlighter } from 'react-syntax-highlighter';
 import js from 'react-syntax-highlighter/dist/esm/languages/hljs/javascript';
 import hybrid from 'react-syntax-highlighter/dist/esm/styles/hljs/hybrid';
-
-(() => {
-  SyntaxHighlighter.registerLanguage('javascript', js);
-  const link = document.createElement('link');
-  link.href = '//cdn.jsdelivr.net/npm/hack-font@3/build/web/hack.css';
-  document.body.append(link);
-})();
 
 interface SolutionProps {
   solution: Solution;
 }
 
+SyntaxHighlighter.registerLanguage('javascript', js);
+
+const GlobalStyle = createGlobalStyle`
+  code {
+    font-family: 'Inconsolata', sans-serif;
+  }
+`;
+
 const CodeMirror = ({ solution }: SolutionProps) => {
   const { code, selectedLanguage } = solution;
   return (
-    <CodeStyle>
-      <CopyClipBoardButton codeText={code} />
-      <SyntaxHighlighter
-        language={selectedLanguage}
-        style={hybrid}
-        lineProps={{
-          style: {
-            fontFamily: 'Hack, sans-serif',
-            fontWeight: '400',
-            fontSize: '1.05rem',
-            lineHeight: '1.5rem',
-          },
-        }}
-        customStyle={{ padding: '1rem', borderRadius: '0.2rem' }}
-        wrapLines={true}
-      >
-        {code}
-      </SyntaxHighlighter>
-    </CodeStyle>
+    <>
+      <GlobalStyle />
+      <CodeStyle>
+        <CopyClipBoardButton codeText={code} />
+        <SyntaxHighlighter
+          language={selectedLanguage}
+          style={hybrid}
+          lineProps={{
+            style: {
+              fontSize: '1.05rem',
+              lineHeight: '1.5rem',
+              letterSpacing: '0.1rem',
+            },
+          }}
+          customStyle={{ padding: '1rem', borderRadius: '0.2rem' }}
+          wrapLines={true}
+        >
+          {code}
+        </SyntaxHighlighter>
+      </CodeStyle>
+    </>
   );
 };
 
