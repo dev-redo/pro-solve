ac93d348873fe11bcc701aa2547eaf809f250fc0
dev-redo <mis05041@naver.com>
2023-02-26 05:35:22 +0900

    feat: 각 스토리의 argument와 component에 대한 description 작성

--- src/stories/components/shared/section/Pagination.stories.tsx
+++ src/stories/components/shared/section/Pagination.stories.tsx
@@ -10,21 +10,35 @@ export default {
     total: {
       control: { type: 'number', min: 0 },
       defaultValue: 100,
+      description: '총 데이터의 개수',
     },
     limit: {
       control: { type: 'number', min: 1 },
       defaultValue: 10,
+      description: '한 페이지당 보여줄 아이템의 개수',
     },
     unit: {
       control: { type: 'number', min: 1 },
       defaultValue: 5,
+      description: '페이지 버튼 그룹의 크기',
     },
     pageIdx: {
       control: { type: 'number', min: 0 },
       defaultValue: 0,
+      description: '현재 선택된 페이지의 인덱스 (0부터 시작)',
     },
     onChangePageIdx: {
       control: null,
+      description: '페이지 버튼을 클릭했을 때 페이지 인덱스를 변경하는 함수',
+    },
+  },
+  parameters: {
+    docs: {
+      description: {
+        component: `이 페이지네이션 컴포넌트는 총 데이터의 개수, 한 페이지당 보여줄 아이템의 개수, 페이지 버튼 그룹의 크기 등을 설정하여 페이지네이션 UI를 보여줍니다. <br />
+        페이지 버튼 그룹의 크기가 클수록 한 번에 보여지는 페이지 버튼이 많아집니다. <br />
+        페이지 버튼을 클릭하면 onChangePageIdx 콜백 함수가 호출되며, 페이지 인덱스를 변경하여 다른 페이지를 보여줄 수 있습니다.`,
+      },
     },
   },
 } as ComponentMeta<typeof Pagination>;
--- src/stories/pages/solution/SolutionTab.stories.tsx
+++ src/stories/pages/solution/SolutionTab.stories.tsx
@@ -12,6 +12,34 @@ import { loginMockData, logoutMockData } from './mock-data';
 export default {
   title: 'Page/SolutionTab',
   component: SolutionTab,
+  argTypes: {
+    selectedLanguage: {
+      control: { type: 'string' },
+      description: '사용자가 선택한 언어',
+    },
+    problemName: {
+      control: { type: 'string' },
+      description: '문제 이름',
+    },
+    isLoaded: {
+      control: { type: 'boolean' },
+      defaultValue: false,
+      description: '데이터 로딩 여부',
+    },
+    solutions: {
+      control: null,
+      defaultValue: loginMockData,
+      description: '사용자가 제출한 풀이 리스트',
+    },
+  },
+  parameters: {
+    docs: {
+      description: {
+        component: `SolutionTab 컴포넌트는 사용자가 제출한 풀이들을 보여주는 페이지로, 사용자가 선택한 언어로 푼 풀이들을 보여주며 Select 컴포넌트들을 이용해 필터링을 할 수 있습니다. <br /> 
+        각 풀이에는 문제 이름, 사용자 이름, 코드, 작성일 등이 포함되어 있습니다. 또한, 풀이의 성공 여부를 색으로 구분하여 표시하여 사용자가 한눈에 풀이 상태를 파악할 수 있도록 합니다.`,
+      },
+    },
+  },
 } as ComponentMeta<typeof SolutionTab>;
 
 interface TemplateInterface {
